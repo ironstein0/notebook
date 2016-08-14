69ebab6354b15067c650d3aed0c753085b786e72
RiyanshKarani011235 <riyansh.karani.011235@gmail.com>
2016-08-15 01:22:45 +0530

    started using jasmine framework for testing

---
+++ test/jasmine/spec/BoxSpec.js
@@ -0,0 +1,13 @@
+describe("Box", function() {
+    it("just trying this out", function() {
+        expect(true).toEqual(true);
+    });
+
+    it("just another try", function() {
+        expect(true).toEqual(false);
+    });
+
+    it("just one more try", function() {
+        expect(false).toEqual(true);
+    });
+});
